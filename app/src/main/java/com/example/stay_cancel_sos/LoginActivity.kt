ca97db5bf8f3bf4da8c021fe426cb5b998ab489d
ha-es <dmstj00423@naver.com>
2023-02-24 16:53:53 +0900

    firebase 등록

--- app/src/main/java/com/example/stay_cancel_sos/LoginActivity.kt
+++ app/src/main/java/com/example/stay_cancel_sos/LoginActivity.kt
@@ -3,21 +3,20 @@ package com.example.stay_cancel_sos
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
 
-
+class LoginActivity : AppCompatActivity() {
+    private var auth : FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
 
     }
-
 }
