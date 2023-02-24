47fe6b256a12ae4d6f5fac361ba022f82bdb9444
ha-es <dmstj00423@naver.com>
2023-02-24 21:39:29 +0900

    firebase 회원가입

--- app/src/main/java/com/example/stay_cancel_sos/LoginActivity.kt
+++ app/src/main/java/com/example/stay_cancel_sos/LoginActivity.kt
@@ -1,22 +1,63 @@
 package com.example.stay_cancel_sos
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 
 class LoginActivity : AppCompatActivity() {
-    private var auth : FirebaseAuth? = null
+    lateinit var emailEt: EditText
+    lateinit var passwordEt: EditText
+    lateinit var loginBtn: Button
+
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance()
 
+        emailEt = findViewById(R.id.Email_et)
+        passwordEt = findViewById(R.id.Pwd_et)
+        loginBtn = findViewById(R.id.button)
+
+        loginBtn.setOnClickListener {
+            var email = emailEt.text.toString()
+            var password = passwordEt.text.toString()
+
+            auth.createUserWithEmailAndPassword(email,password) // 회원 가입
+                .addOnCompleteListener {
+                        result ->
+                    if(result.isSuccessful){
+                        Toast.makeText(this,"회원가입이 완료되었습니다.",Toast.LENGTH_SHORT).show()
+                        if(auth.currentUser!=null){
+                            var intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
+                    else if(result.exception?.message.isNullOrEmpty()){
+                        Toast.makeText(this,"오류가 발생했습니다.",Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        login(email,password)
+                    }
+                }
+        }
+    }
+
+    fun login(email:String,password:String){
+        auth.signInWithEmailAndPassword(email,password) // 로그인
+            .addOnCompleteListener {
+                    result->
+                if(result.isSuccessful){
+                    var intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
     }
 }
--- app/src/main/java/com/example/stay_cancel_sos/MainActivity.kt
+++ app/src/main/java/com/example/stay_cancel_sos/MainActivity.kt
@@ -1,22 +1,24 @@
 package com.example.stay_cancel_sos
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
-
+    // 로그인
+    lateinit var email: TextView
+    lateinit var auth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 로그인
+        auth = FirebaseAuth.getInstance()
+        email = findViewById(R.id.email_tv)
+        email.text = auth.currentUser?.email
 
     }
 
