cbbe529a66e13774d5da61058722ae35e19b8f38
gustmdqkr321 <mbc3940@kookmin.ac.kr>
2023-02-27 21:57:53 +0900

    채팅 목록 UI

---
+++ app/src/main/java/com/example/stay_cancel_sos/ChatActivity.kt
@@ -0,0 +1,17 @@
+package com.example.stay_cancel_sos
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
--- app/src/main/java/com/example/stay_cancel_sos/MainActivity.kt
+++ app/src/main/java/com/example/stay_cancel_sos/MainActivity.kt
@@ -3,16 +3,11 @@ package com.example.stay_cancel_sos
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
-import android.view.MenuItem
 import android.widget.Button
-import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-
-
-
 
 class MainActivity : AppCompatActivity() {
     lateinit var email: TextView
@@ -20,15 +15,20 @@ class MainActivity : AppCompatActivity() {
     lateinit var auth:FirebaseAuth
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
 
         auth = FirebaseAuth.getInstance()
         email = findViewById(R.id.email_tv)
 
         email.text = auth.currentUser?.email
+
+
+
 
 
 
