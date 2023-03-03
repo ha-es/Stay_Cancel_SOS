dc429e26747f4313f3aaf0f2824fdfca57415ab7
EunSeo Ha <121787902+ha-es@users.noreply.github.com>
2023-03-03 21:38:45 +0900

    toggleBtn 수정

--- app/src/main/java/com/example/stay_cancel_sos/ProfileDetailActivity.kt
+++ app/src/main/java/com/example/stay_cancel_sos/ProfileDetailActivity.kt
@@ -2,17 +2,25 @@ package com.example.stay_cancel_sos
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.BounceInterpolator
+import android.view.animation.ScaleAnimation
 import android.widget.Button
+import android.widget.CompoundButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_profile_detail.*
+
 
 class ProfileDetailActivity : AppCompatActivity() {
     lateinit var datas : ProfileData
     lateinit var reg_btn : Button
     lateinit var stay_btn : Button
 
+    lateinit var scaleAnimation: ScaleAnimation
+    lateinit var bounceInterpolator : BounceInterpolator
+    lateinit var button_heart: CompoundButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +30,30 @@ class ProfileDetailActivity : AppCompatActivity() {
         stay_btn = findViewById(R.id.stay_btn)
         reg_btn.setClickable(false)
         stay_btn.setClickable(false)
-
-
         val type1 = "펜션"
+
+        button_heart = findViewById(R.id.button_heart)
+        button_heart.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { compoundButton, isChecked ->
+            compoundButton.startAnimation(
+                scaleAnimation
+            )
+        })
+
+        /* togglebtn(button_heart) */
+        scaleAnimation = ScaleAnimation(
+            0.7f,
+            1.0f,
+            0.7f,
+            1.0f,
+            Animation.RELATIVE_TO_SELF,
+            0.7f,
+            Animation.RELATIVE_TO_SELF,
+            0.7f
+        )
+        scaleAnimation.duration =500
+        bounceInterpolator = BounceInterpolator()
+        scaleAnimation.interpolator = bounceInterpolator
+
 
         datas = intent.getSerializableExtra("data") as ProfileData
 
